257b605d5eeab9f94fa0959815ba7826561fe8fc
Charlotte Buckley <cfbuckley14@gmail.com>
2022-03-30 12:22:17 +0100

    adding structure of home page

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+<head>
+    <title>Charlotte Buckley - Portfolio</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="normalize.css">
+    <script src="script.js"></script>
+    <link rel="stylesheet" type ="text/css" href="index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
+</head>
+<body class="home-page">
+<nav>
+<!--    <div>-->
+<!--        <img id="burger-menu-icon" src="icons/burger.svg" alt="nav menu"/>-->
+<!--    </div>-->
+<!--    <ul class="burger-menu">-->
+<!--        <li class="burger-menu-items"><a href="about.html">about</a></li>-->
+<!--        <li class="burger-menu-items"><a href="work.html">work</a></li>-->
+<!--    </ul>-->
+    <div class="nav-icons">
+        <a href="https://www.linkedin.com/in/charlotte-buckley-93866416a/">
+            <img src="icons/linkedin.svg" alt="LinkedIn Icon"/>
+        </a>
+        <a href="https://github.com/cfbuckley14">
+            <img src="icons/github.svg" alt="GitHub Icon"/>
+        </a>
+        <a href="mailto:cfbuckley14@gmail.com" >
+            <img src="icons/mail.svg" alt="Email Icon"/>
+        </a>
+    </div>
+</nav>
+<main>
+    <header>
+        <div class="header-container">
+            <p>Hi there, I'm</p>
+            <h1>Charlotte.</h1>
+            <div>
+                <h2 class="normal-text">A full-stack developer.</h2>
+                <p>Find out more about me and my journey.</p>
+            </div>
+            <div class="social-icons">
+                <a href="https://www.linkedin.com/in/charlotte-buckley-93866416a/">
+                    <img src="icons/linkedin.svg" alt="LinkedIn Icon"/>
+                </a>
+                <a href="https://github.com/cfbuckley14">
+                    <img src="icons/github.svg" alt="GitHub Icon"/>
+                </a>
+                <a href="mailto:cfbuckley14@gmail.com" >
+                    <img src="icons/mail.svg" alt="Email Icon"/>
+                </a>
+            </div>
+        </div>
+    </header>
+</main>
+</body>
+</html>
+
