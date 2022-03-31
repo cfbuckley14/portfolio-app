7c91ae5689668a2d1542f87ddd7455c6c1ca6e06
Charlotte Buckley <cfbuckley14@gmail.com>
2022-03-31 20:35:14 +0100

    final updates for burger menu across all pages

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-function openCloseBurgerMenu(button, menuItems) {
+function openCloseBurgerMenu(button, menuItems, overlay) {
     document.querySelector(button).addEventListener('click', () => {
         if(document.querySelector(menuItems).classList.contains('hidden')) {
             document.querySelector(menuItems).classList.remove('hidden')
+            document.querySelector(overlay).classList.add('overlay')
         } else {
             document.querySelector(menuItems).classList.add('hidden')
+            document.querySelector(overlay).classList.remove('overlay')
         }
     })
 }
 
-openCloseBurgerMenu('#burger-menu-button', '#burger-menu')
+openCloseBurgerMenu('#burger-menu-button', '#burger-menu', '#overlay')
 
